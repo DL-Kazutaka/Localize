29df1431fad5d2d0eadb5d22e086b93a09d195d4
kazutaka.matsusaki <kazutaka.matsusaki@dartslive.com>
2013-11-18 15:16:39 +0900

    Add iOS localize project.

---
+++ iOS_xcode/iOS_xcode/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS_xcode
+//
+//  Created by DLGames on 2013/11/18.
+//  Copyright (c) 2013年 DLGames. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS_xcode/iOS_xcode/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iOS_xcode
+//
+//  Created by DLGames on 2013/11/18.
+//  Copyright (c) 2013年 DLGames. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
